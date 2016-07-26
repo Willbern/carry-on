3d63cccc576530e8872deb6825bf7d3b8477b8c9
Will <zhguo@dataman-inc.com>
2016-07-26 17:24:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"time"
+//	"fmt"
+//	"time"
+//  "os"
 )
 
 func main() {
-	for i := 0; i < 1000000; i++ {
-		fmt.Println("keep calm and carry on")
-		fmt.Println("no zuo no die")
-		time.Sleep(time.Second * 10)
-	}
+	//for i := 0; i < 1000000; i++ {
+	//	fmt.Println("keep calm and carry on")
+	//	fmt.Println("no zuo no die")
+	//	time.Sleep(time.Second * 10)
+	//}
+	panic(-1)
+	// os.Exit(0)
 }
